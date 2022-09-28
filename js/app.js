09cbc0309adad95f42c520175d1b1314d65ccc0c
YA <YA-androidapp@users.noreply.github.com>
2022-09-28 20:08:24 +0900

    init

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2022 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('guid').value = uuid();
+});
